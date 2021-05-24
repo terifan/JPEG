667986bd549276a3a349aa4344a954e10d272ba9
terifan <pinjata69@gmail.com>
2021-05-24 10:36:00 +0200

    update

--- src/examples/TranscodeJPEGDemo.java
+++ src/examples/TranscodeJPEGDemo.java
@@ -1,10 +1,8 @@
 package examples;
 
 import java.io.File;
-import java.net.URL;
-import org.terifan.imageio.jpeg.JPEGImageIO;
-import examples.res.R;
 import org.terifan.imageio.jpeg.CompressionType;
+import org.terifan.imageio.jpeg.JPEGImageIO;
 
 
 public class TranscodeJPEGDemo
@@ -13,12 +11,12 @@ public class TranscodeJPEGDemo
 	{
 		try
 		{
-			URL input = R.class.getResource("Swallowtail.jpg");
-//			File input = new File("d:\\desktop\\ad6e9852-9c3e-43ec-832d-3b0e52abbecb.jpg");
+//			URL input = R.class.getResource("Swallowtail.jpg");
+			File input = new File("D:\\Pictures\\Wallpapers Fantasy\\dragon-wallpaper-1920x1080-1009013.jpg");
 
 			File output = new File("d:\\Swallowtail-arithmetic.jpg");
 
-			new JPEGImageIO().setCompressionType(CompressionType.Huffman).transcode(input, output);
+			new JPEGImageIO().setCompressionType(CompressionType.ArithmeticProgressive).transcode(input, output);
 
 			_ImageWindow.show(output).setTitle("" + output.length());
 		}
