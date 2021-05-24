c6eb2734eee9ba88ff163e6edf8c88dbcd20be3d
terifan <pinjata69@gmail.com>
2021-05-24 10:35:49 +0200

    update

--- src/examples/CoefficientAccessDemo.java
+++ src/examples/CoefficientAccessDemo.java
@@ -2,6 +2,7 @@ package examples;
 
 import examples.res.R;
 import java.io.ByteArrayOutputStream;
+import java.util.Random;
 import org.terifan.imageio.jpeg.JPEG;
 import org.terifan.imageio.jpeg.JPEGImageIO;
 
@@ -17,39 +18,57 @@ public class CoefficientAccessDemo
 		{
 			JPEG input = new JPEGImageIO().decode(R.class.getResource("Swallowtail.jpg"));
 
-			for (int loop = 0; loop < 2; loop++)
+			int[][][][] srcCoefficients = input.getCoefficients();
+
+			int w = srcCoefficients.length;
+			int h = srcCoefficients[0].length;
+			int d = srcCoefficients[0][0].length;
+
+			int numcu = w * h * d;
+
+			int[] order = new int[numcu];
+			int[] reorder = new int[numcu];
+			for (int i = 0; i < numcu; i++)
 			{
-				int[][][][] coefficients = input.getCoefficients();
-
-				for (int i = 0; i < coefficients.length; i++)
-				{
-					for (int j = 0; j < coefficients[i].length; j++)
-					{
-						int[] b0 = coefficients[i][j][0];
-						int[] b1 = coefficients[i][j][1];
-						int[] b2 = coefficients[i][j][2];
-						int[] b3 = coefficients[i][j][3];
-
-						coefficients[i][j][0] = b3;
-						coefficients[i][j][1] = b2;
-						coefficients[i][j][2] = b1;
-						coefficients[i][j][3] = b0;
-					}
-				}
-
-				JPEG output = new JPEG();
-				output.mCoefficients = coefficients;
-				output.mColorSpace = input.mColorSpace;
-				output.mQuantizationTables = input.mQuantizationTables;
-				output.mSOFSegment = input.mSOFSegment;
-
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				new JPEGImageIO().encode(output, baos);
-
-				_ImageWindow.show(new JPEGImageIO().read(baos.toByteArray()));
-
-				input = output;
+				order[i] = i;
 			}
+
+			Random rnd = new Random();
+			for (int i = 0; i < numcu; i++)
+			{
+				int j = rnd.nextInt(numcu);
+				int tmp = order[j];
+				order[j] = order[i];
+				order[i] = tmp;
+			}
+
+			int[][][][] dstCoefficients = new int[w][h][d][];
+
+			for (int i = 0; i < numcu; i++)
+			{
+				int srcO = i;
+				int si = srcO % w;
+				int sj = (srcO / w) % h;
+				int sk = (srcO / w / h) % d;
+
+				int dstO = order[i];
+				int di = dstO % w;
+				int dj = (dstO / w) % h;
+				int dk = (dstO / w / h) % d;
+
+				dstCoefficients[di][dj][dk] = srcCoefficients[si][sj][sk];
+			}
+
+			JPEG output = new JPEG();
+			output.mCoefficients = dstCoefficients;
+			output.mColorSpace = input.mColorSpace;
+			output.mQuantizationTables = input.mQuantizationTables;
+			output.mSOFSegment = input.mSOFSegment;
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			new JPEGImageIO().encode(output, baos);
+
+			_ImageWindow.show(new JPEGImageIO().read(baos.toByteArray()));
 		}
 		catch (Throwable e)
 		{
