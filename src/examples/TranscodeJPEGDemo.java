2849108b29d0c6a7a5300dad2181a312d447490b
terifan <pinjata69@gmail.com>
2026-01-14 16:01:18 +0100

    update

--- src/examples/TranscodeJPEGDemo.java
+++ src/examples/TranscodeJPEGDemo.java
@@ -23,8 +23,8 @@ public class TranscodeJPEGDemo
 			File output4 = new File(dest, "jpg-huff-opt\\" + filename);
 			File output5 = new File(dest, "jpg-huff-prog\\" + filename);
 
-			new JPEGImageIO().setLog(System.out).setCompressionType(CompressionType.Arithmetic).transcode(input, output1);
-			new JPEGImageIO().setCompressionType(CompressionType.ArithmeticProgressive).transcode(input, output2);
+			new JPEGImageIO().setCompressionType(CompressionType.Arithmetic).transcode(input, output1);
+			new JPEGImageIO().setLog(System.out).setCompressionType(CompressionType.ArithmeticProgressive).transcode(input, output2);
 			new JPEGImageIO().setCompressionType(CompressionType.Huffman).transcode(input, output3);
 			new JPEGImageIO().setCompressionType(CompressionType.HuffmanOptimized).transcode(input, output4);
 			new JPEGImageIO().setCompressionType(CompressionType.HuffmanProgressive).transcode(input, output5);
